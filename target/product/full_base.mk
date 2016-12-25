7fbe9c016993dc55a5fdfd771421c96c3b9bd1ce
beanstown106 <nbenis106@gmail.com>
2016-12-25 13:40:14 -0500

    GoogleAudio: set new pixel sounds as defaults [2/2]
    Change-Id: I7fe60f94726de9c1588f6e06fb112e42915dea0e

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -23,6 +23,11 @@ PRODUCT_PACKAGES := \
     libfwdlockengine \
     WAPPushManager
 
+# Additional settings used in all AOSP builds
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.config.ringtone=Zen.ogg \
+    ro.config.notification_sound=Chime.ogg
+
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
 
