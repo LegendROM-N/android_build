585b18554596d140287b41ba8e6b3332ace1f4ed
Yashraj Jhala <jhalayashraj@gmail.com>
2017-01-02 21:51:50 +0530

    7.1.1

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -682,7 +682,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print("L::::::::::::::::::::::LR::::::R     R:::::R");
   script.Print("LLLLLLLLLLLLLLLLLLLLLLLLRRRRRRRR     RRRRRRR");
   script.Print("")
-  script.Print("          ||| ANDROID 7.1 |||           ")
+  script.Print("          ||| ANDROID 7.1.1 |||           ")
   script.Print("")
 
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
